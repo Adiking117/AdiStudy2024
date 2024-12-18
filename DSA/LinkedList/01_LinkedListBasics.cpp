8b08a8dbadc161365e392b59859c5fd127344a70
[Aditya <[www.aditya117@gmail.com]>
2024-12-18 16:56:18 +0530

    linked list done

--- DSA/LinkedList/01_LinkedListBasics.cpp
+++ DSA/LinkedList/01_LinkedListBasics.cpp
@@ -10,6 +10,10 @@ struct Node{
         data = dataValue;
         next = nextAddress;
     }
+    Node(int dataValue){
+        data = dataValue;
+        next = nullptr;
+    }
 };
 
 Node* convertArr2LL(vector<int> &arr){
@@ -26,10 +30,11 @@ Node* convertArr2LL(vector<int> &arr){
 void printLinkedList(Node* head,int* count){
     Node* mover = head;
     while(mover){
-        cout<< mover->data << " " << endl;
+        cout<< mover->data << " " ;
         mover = mover->next;
         (*count)++;
     }
+    cout<< endl;
 }
 
 bool SearchInLL(Node* head , int value){
@@ -41,7 +46,109 @@ bool SearchInLL(Node* head , int value){
     return false;
 }
 
+Node* InsertKThElement(Node* head, int valueToBeInserted , int k){
+    if(head == nullptr){
+        if(k == 1){
+            return new Node(valueToBeInserted);
+        }else{
+            return head;
+        }
+    }
 
+    if(k == 1){
+        return new Node(valueToBeInserted,head);
+    }
+
+    int count = 0;
+    Node* temp = head;
+    while(temp){
+        count++;
+        if(count == k-1){
+            Node* newNodeToInsert = new Node(valueToBeInserted,temp->next);
+            temp->next = newNodeToInsert;
+            break;
+        }
+        temp = temp->next;
+    }
+    
+    return head;
+
+}
+
+Node* InsertBeforeValue(Node* head, int valueToBeInserted , int v){
+    if(head == nullptr){
+        return nullptr;
+    }
+
+    if(head->data == v){
+        return new Node(valueToBeInserted,head);
+    }
+
+    Node* temp = head;
+    while(temp->next){
+        if(temp->next->data == v){
+            Node* newNodeToInsert = new Node(valueToBeInserted,temp->next);
+            temp->next = newNodeToInsert;
+            break;
+        }
+        temp = temp->next;
+    }
+    
+    return head;
+
+}
+
+
+Node* DeleteKThElement(Node* head, int k){
+    if(head == nullptr) 
+        return nullptr;
+    if(k == 1){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    int count = 0;
+    Node* temp = head;
+    Node* prev = nullptr;
+    while(temp){
+        count++;
+        if(count == k){
+            prev->next = temp->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    
+    return head;
+}
+
+Node* DeleteByValue(Node* head, int v){
+    if(head == nullptr) 
+        return nullptr;
+        
+    if(head->data == v){
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    Node* temp = head;
+    Node* prev = nullptr;
+    while(temp){
+        if(temp->data == v){
+            prev->next = temp->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    
+    return head;
+}
 
 int main(){
     vector<int> arr = {10,20,30,40,50};
@@ -49,8 +156,22 @@ int main(){
 
     int count = 0;
     printLinkedList(head,&count);
-    cout<< "The length of LL "<< count << endl;
+    // cout<< "The length of LL "<< count << endl;
     
-    cout << SearchInLL(head,30) << endl;
+    // cout << SearchInLL(head,30) << endl;
+
+    head = InsertKThElement(head,25,1);
+    printLinkedList(head,&count);
+
+    head = InsertBeforeValue(head,35,40);
+    printLinkedList(head,&count);
+
+    head = DeleteKThElement(head,4);
+    printLinkedList(head,&count);
+    
+    head = DeleteByValue(head,40);
+    printLinkedList(head,&count);
+
+
     return 1;
 }
